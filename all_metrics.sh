c88c8435a0c189665c5b852affbaa1a7372442bd
Roman <roman.1e2f5p8s.over3@gmail.com>
2022-07-11 15:34:26 +0200

    Transferring code to reomte machine

---
+++ all_metrics.sh
@@ -0,0 +1,11 @@
+./metric.py --dir='data/tokens' --N=1000 --metric='entropy' --latex
+./metric.py --dir='data/tokens' --N=1000 --metric='gini' --latex
+./metric.py --dir='data/tokens' --N=1000 --metric='nakamoto' --latex
+./metric.py --dir='data/tokens' --N=1000 --metric='robin' --latex
+./metric.py --dir='data/tokens' --N=1000 --metric='efficiency' --latex
+
+./metric.py --dir='data/tokens' --N=10000 --metric='entropy' --latex
+./metric.py --dir='data/tokens' --N=10000 --metric='gini' --latex
+./metric.py --dir='data/tokens' --N=10000 --metric='nakamoto' --latex
+./metric.py --dir='data/tokens' --N=10000 --metric='robin' --latex
+./metric.py --dir='data/tokens' --N=10000 --metric='efficiency' --latex
--- metric.py
+++ metric.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3.9
+
 import os
 import argparse
 import datetime
@@ -11,39 +13,17 @@ import matplotlib.dates as mdates
 import matplotlib.cm as cm
 from time import time
 
-prev_sum = 0
-prev_N = 0
-
 
 def entropy(x):
     return -np.dot(x, np.log(x))
 
 
-def gini(x, prev_sum=0, prev_N=0):
-    # n = len(x)
-
-    # method
-    new = 2 * np.abs(np.tril(np.subtract.outer(x[prev_N:], x[prev_N:]))).sum()
-    # cross = 2 * sum([np.abs(val - x[:prev_N]).sum() for val in x[prev_N:]])
-    # print(mat)
-    cross = 2 * np.abs(np.subtract.outer(x[prev_N:], x[:prev_N])).sum()
-    # print('prev_sum', prev_sum, 'new', new, 'cross', cross)
-    return prev_sum + new + cross
-
-    # method
-    # return 2 * np.abs(np.tril(np.subtract.outer(x, x))).sum()
-
-    # method
+def gini(x):
     # s = 0
     # for i in range(len(x)):
         # s += np.abs(x[i] - x).sum()
-    # return s
-
-    # method
-    # return sum([np.abs(val - x).sum() for val in x]) # very slow, O(n^2)
-
-    # this one does not work
-    # return np.array([i * x[i] - (n - 1 - i) * x[i] for i in range(n - 1, -1, -1)]).sum() / n
+    # return s / (2 * len(x))
+    return np.abs(np.tril(np.subtract.outer(x, x))).sum() / len(x)
 
 
 def nakamoto(x):
@@ -79,35 +59,17 @@ required_args.add_argument(
         help='Path to parent directory with coins/tokens data',
         )
 required_args.add_argument(
-        '--name',
-        type=str,
-        required=True,
-        help='Name of coin/token',
-        )
-required_args.add_argument(
-        '--min_N',
-        type=int,
-        required=True,
-        help='Minimum number of top addresses to consider',
-        )
-required_args.add_argument(
-        '--max_N',
-        type=int,
-        required=True,
-        help='Maximum number of top addresses to consider',
-        )
-required_args.add_argument(
-        '--step_N',
-        type=int,
-        required=True,
-        help='Step for the top addresses',
-        )
-required_args.add_argument(
         '--metric',
         type=str,
         choices=['entropy', 'gini', 'nakamoto', 'efficiency', 'robin'],
         required=True,
         help='Metric to plot. Available metrics: entropy, '
+        )
+required_args.add_argument(
+        '--N',
+        type=int,
+        required=True,
+        help='Top N token holders to consider',
         )
 
 # optimal arguments
@@ -125,6 +87,12 @@ optional_args.add_argument(
         help='Print detailed output to console, defaults to False'
         )
 optional_args.add_argument(
+        '--top',
+        type=int,
+        default=10000,
+        help='How many top holders to consider, defaults to 10000',
+        )
+optional_args.add_argument(
         '--latex',
         action='store_true',
         default=False,
@@ -138,102 +106,17 @@ optional_args.add_argument(
         )
 args = parser.parse_args()
 
-
-DIR = os.path.join(args.dir, args.name)
-# fname = [x for x in os.listdir(DIR) if 'csv' in x and not 'addresses' in x and not
-        # x.split('.')[0].isdigit()][0]
-fname = args.name + '.csv'
-df = pd.read_csv(os.path.join(DIR, fname), header=0, index_col=0)
-df.drop(df.index[0], inplace=True)
-
-
-Y = np.arange(args.min_N, args.max_N + args.step_N, args.step_N)
-mat = []
-
-FIRST_COL = None
-for i, col in enumerate(df):
-    if not df[col][:args.max_N].isnull().sum():
-        FIRST_COL = col
-        break
-# print(df)
-# print(df.loc[:, FIRST_COL:])
-# print(FIRST_COL)
-# # exit()
-non_nan_df = df.loc[:, FIRST_COL:].copy()
-# print(non_nan_df)
-
-# some token addresses have zero or nagative balance, this may cause warning when computing entropy
-# so we will replace 'bad' values with machine epsilon
-for col in non_nan_df:
-    non_nan_df.loc[np.isclose(non_nan_df[col], 0), col] = np.finfo(float).eps
-# print(non_nan_df)
-
-N_WEEKS = non_nan_df.shape[-1]
-X = pd.to_datetime(non_nan_df.columns)
-if args.verbose:
-    print('Total number of weeks: {}'.format(df.shape[-1]))
-    print('Number of weeks with more than {} addresses: {}'.format(args.max_N, N_WEEKS))
-    print('Number of weeks to be dropped: {}'.format(df.shape[-1] - N_WEEKS))
-
-
-
-start = time()
-for i, col in enumerate(non_nan_df):
-    if args.verbose:
-        # print('Computing for N={} out of {}'.format(N, args.max_N))
-        print('Computing for week {} out of {}'.format(i, N_WEEKS))
-    y = []
-    # top_N = np.array([])
-    for N in Y:
-        top_N = non_nan_df[col][:N].to_numpy()
-        # top_N = np.append(top_N, np.arange(N-args.step_N+1, N+1, 1))
-        # print(N, top_N)
-        
-        if args.metric == 'entropy':
-            top_N = top_N / top_N.sum()
-            assert np.isclose(top_N.sum(), 1)
-            y.append(entropy(top_N))
-        elif args.metric == 'gini':
-            # top_N = np.array([1, 8, 9, 15, 16])
-            # print(prev_sum / (2*(N-args.step_N)), end=' ')
-            # top_N = top_N / top_N.sum()
-            # assert np.isclose(top_N.sum(), 1)
-            prev_sum = gini(top_N, prev_sum=prev_sum, prev_N=prev_N)
-            prev_N = N
-            y.append(prev_sum / (2 * N * top_N.sum()))
-            # y.append(prev_sum)
-            # print(y[-1])
-            # print(prev_sum)
-            # exit()
-        elif args.metric == 'nakamoto':
-            top_N = top_N / top_N.sum()
-            assert np.isclose(top_N.sum(), 1)
-            y.append(nakamoto(top_N))
-        elif args.metric == 'efficiency':
-            top_N = top_N / top_N.sum()
-            assert np.isclose(top_N.sum(), 1)
-            y.append(efficiency(top_N))
-        elif args.metric == 'robin':
-            top_N = top_N / top_N.sum()
-            assert np.isclose(top_N.sum(), 1)
-            y.append(robin(top_N))
-        else:
-            pass
-        # exit()
-    
-    # if i == 5:
-        # exit()
-    mat.append(y)
-    prev_sum = 0
-    prev_N = 0
-    # mat = np.array(mat).T
-    # print(mat, np.array(mat).T.shape)
-    # exit()
-if args.verbose:
-    print('Elapsed time: {} s'.format(time() - start))
-
-mat = np.array(mat).T
-x_lim = mdates.date2num([X[0], X[-1]])
+LABELS = {
+        'ChainLink Token': 'LINK',
+        'Dai Stablecoin': 'DAI',
+        'Matic Token': 'MATIC',
+        'stETH': 'stETH',
+        'Tether USD': 'USDT',
+        'Theta Token': 'THETA',
+        'Uniswap (UNI)': 'UNI',
+        'USD Coin': 'USDC',
+        'Wrapped Bitcoin (WBTC)': 'WBTC'
+        }
 
 if args.latex:
     plt.rcParams['font.size'] = 20
@@ -243,84 +126,70 @@ if args.latex:
 
 fig, ax = plt.subplots(figsize=(8.636, 5.2)) # this width is twice larger for double-column IEEE articles
 
-COLOR_MAP = mcol.LinearSegmentedColormap.from_list(
-        name='custom_cmap',
-        # colors=['#f5f0e1', '#ff6e40', '#ffc13b', '#1e3d69'],
-        colors=['#9400D3', '#4B0082', '#0000FF', '#00FF00', '#FFFF00', '#FF7F00', '#FF0000'],
-        )
-COLOR_NORMALIZER = mcol.Normalize(
-        vmin=mat.min().min(),  # red
-        vmax=mat.max().max(),  # blue
-        )
-COLOR_PICKER = cm.ScalarMappable(
-        norm=COLOR_NORMALIZER,
-        cmap=COLOR_MAP,
-        )
-COLOR_PICKER.set_array([])
+token_csvs = [f for f in os.listdir(args.dir) if 'csv' in f]
+for token_csv in token_csvs:
+    # if token_csv in ['Theta Token.csv', 'stETH.csv']:
+        # continue
 
-im = ax.imshow(
-        X=np.flip(mat, axis=0),
-        cmap=COLOR_MAP,
-        # cmap=cm.get_cmap('hsv'),
-        extent=(x_lim[0]+1, x_lim[-1], Y[0], Y[-1]),
-        aspect='auto',
-        # norm=mcol.LogNorm(),
-        )
+    df = pd.read_csv(os.path.join(args.dir, token_csv), header=0, index_col=0)
+    df.drop(df.index[0], inplace=True)
 
-label = '{}'.format(args.metric.title())
-if args.metric in ['gini', 'nakamoto']:
-    label += ' coefficient'
-elif args.metric == 'robin':
-    label += ' Hood index'
+    FIRST_COL = None
+    for i, col in enumerate(df):
+        if not df[col][:args.top].isnull().sum():
+            FIRST_COL = col
+            break
+    non_nan_df = df.loc[:, FIRST_COL:].copy()
 
-fig.colorbar(
-        im,
-        ax=ax,
-        label=label,
-        )
-# plt.hsv()
+    name = token_csv.split('.')[0]
+    print('First {} weeks dropped for \"{}\"'.format(df.shape[-1] - non_nan_df.shape[-1], name))
 
-title = label + ' for {}'.format(args.name)
-ax.set_title(title)
+    X = pd.to_datetime(non_nan_df.columns)
+    Y = []
+
+    for i, col in enumerate(non_nan_df):
+        # some token addresses have zero or nagative balance, this may cause warning when 
+        # computing entropy so we will replace 'bad' values with machine epsilon
+        non_nan_df.loc[np.isclose(non_nan_df[col], 0), col] = np.finfo(float).eps
+
+        top_N = non_nan_df[col][:args.N].to_numpy()
+        top_N = top_N / top_N.sum()
+        assert np.isclose(top_N.sum(), 1)
+        
+        if args.metric == 'entropy':
+            Y.append(entropy(top_N))
+        elif args.metric == 'gini':
+            Y.append(gini(top_N))
+        elif args.metric == 'nakamoto':
+            Y.append(nakamoto(top_N))
+        elif args.metric == 'efficiency':
+            Y.append(efficiency(top_N))
+        elif args.metric == 'robin':
+            Y.append(robin(top_N))
+        else:
+            pass
+    
+    plt.plot(X, Y, linewidth=3, label=LABELS[name])
+
+ax.set_title('Sample size {}N = {}{}'.format('$' if args.latex else '', args.N,
+    '$' if args.latex else ''))
 
 ax.xaxis_date()
-ax.set_ylabel('Sample size ' + ('$N$' if args.latex else 'N'))
-
+ax.set_xlabel('Year')
 
 if args.ylog:
     plt.yscale('log')
+y_label = '{}'.format(args.metric.title())
+if args.metric in ['gini', 'nakamoto']:
+    y_label += ' coefficient'
+elif args.metric == 'robin':
+    y_label += ' Hood index'
+ax.set_ylabel(y_label)
 
-# myFmt = mdates.DateFormatter('%Y')
-# ax.xaxis.set_major_formatter(myFmt)
-
-date_diff = mdates.num2date(ax.get_xticks()[:2])
-# print(date_diff[1] - date_diff[0])
-if date_diff[-1] - date_diff[0] < datetime.timedelta(days=28):
-    ax.set_xlabel('Year{}Month{}Day'.format('$-$' if args.latex else '-', '$-$' if args.latex else '-'))
-    plt.xticks(
-            rotation=90,
-            horizontalalignment='center',
-            verticalalignment='top',
-            )
-elif date_diff[-1] - date_diff[0] < datetime.timedelta(days=365):
-    ax.set_xlabel('Year{}Month'.format('$-$' if args.latex else '-'))
-    plt.xticks(
-            rotation=90,
-            horizontalalignment='center',
-            verticalalignment='top',
-            )
-else:
-    ax.set_xlabel('Year')
+plt.legend(labelspacing=0.1, fontsize='small')
 
 plt.savefig(
-        os.path.join(DIR, '{}_{}.pdf'.format(args.name, args.metric)),
+        os.path.join(args.dir, 'tokens_{}_N={}.pdf'.format(args.metric, args.N)),
         format='PDF',
         bbox_inches='tight',
         )
-# plt.show()
-
-new_df = pd.DataFrame({'N': Y})
-for i, col in enumerate(non_nan_df):
-    new_df = pd.concat([new_df, pd.DataFrame({col: mat[:, i]})], axis=1)
-
-new_df.to_csv(os.path.join(DIR, '{}_{}.csv'.format(args.name, args.metric)))
